a8db5f986be5f793b46374890e281956352a5219
F3INT H34RTED <68115056+F3INTH34RTED@users.noreply.github.com>
2021-07-21 09:01:29 +0400

    Create 02ReverseInteger.cpp

---
+++ 01Easy/02ReverseInteger.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <chrono>
+
+using std::cout;
+using std::cin;
+
+int reverse(int x)
+{
+
+}
+
+int main()
+{
+    int number = 123, reverseNumber = reverse(number);
+    cout << "Number: " << number << "\tReversed Number: " << reverseNumber << '\n';
+    return 0;
+}
